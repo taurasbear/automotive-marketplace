9c0aeb26ed67d89e6ab79dab4d14b3726d268ee7
taurasbear <youngicorn@gmail.com>
2025-05-08 16:15:52 +0300

    Fixed DOCKER_REGISTRY variable Fixed base url for axios

--- automotive.marketplace.client/src/api/axiosClient.ts
+++ automotive.marketplace.client/src/api/axiosClient.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: '/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    timeout: 0,
-    withCredentials: true
-})
+  baseURL:
+    import.meta.env.VITE_APP_API_URL ??
+    "https://api.automotive_marketplace.taurasbear.me",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 0,
+  withCredentials: true,
+});
 
 export default axiosClient;
