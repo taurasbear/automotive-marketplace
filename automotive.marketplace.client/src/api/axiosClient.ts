73b6f9c589b9dfeffe48686deeab14d07a923ed0
taurasbear <youngicorn@gmail.com>
2025-05-09 15:34:00 +0300

    Set up fail safe for API URL secret missing

--- automotive.marketplace.client/src/api/axiosClient.ts
+++ automotive.marketplace.client/src/api/axiosClient.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL:
-    import.meta.env.VITE_APP_API_URL,
+    import.meta.env.VITE_APP_API_URL ??
+    "https://api.automotive-marketplace.taurasbear.me",
   headers: {
     "Content-Type": "application/json",
   },
