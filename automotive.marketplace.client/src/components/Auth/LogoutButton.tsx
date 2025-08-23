cda23503e48d28072e00d39fbb95c1951cec2be8
taurasbear <youngicorn@gmail.com>
2025-08-23 19:43:23 +0100

    fix: clear auth state on logout

--- automotive.marketplace.client/src/components/Auth/LogoutButton.tsx
+++ automotive.marketplace.client/src/components/Auth/LogoutButton.tsx
@@ -1,11 +1,19 @@
 import { useLogoutAccount } from "@/shared/utils/queries/auth/useLogoutAccount";
 import { Button } from "../ui/button";
+import { useAppDispatch } from "@/shared/hooks/redux";
+import { clearCredentials } from "@/shared/state/authSlice";
 
 const LogoutButton = () => {
+  const dispatch = useAppDispatch();
   const { mutateAsync: logoutAsync } = useLogoutAccount();
 
+  const handleLogout = async () => {
+    await logoutAsync();
+    dispatch(clearCredentials());
+  };
+
   return (
-    <Button variant="secondary" onClick={async () => await logoutAsync()}>
+    <Button variant="secondary" onClick={async () => handleLogout}>
       Logout
     </Button>
   );
