752efb31f17525c418945773f808735ee270c9f1
taurasbear <youngicorn@gmail.com>
2025-08-23 19:43:47 +0100

    refactor: move namespace to go after imports

--- Automotive.Marketplace.Application/Interfaces/Services/ITokenService.cs
+++ Automotive.Marketplace.Application/Interfaces/Services/ITokenService.cs
@@ -1,6 +1,7 @@
-﻿namespace Automotive.Marketplace.Application.Interfaces.Services;
-
+﻿using System.Security.Claims;
 using Automotive.Marketplace.Domain.Entities;
+
+namespace Automotive.Marketplace.Application.Interfaces.Services;
 
 public interface ITokenService
 {
