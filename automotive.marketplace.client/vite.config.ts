4b6db59104c2de3e90fd450ba5e5fbc52621641e
taurasbear <youngicorn@gmail.com>
2025-08-25 16:02:10 +0100

    fix: correctly pass API target env variable for Vite proxy

--- automotive.marketplace.client/vite.config.ts
+++ automotive.marketplace.client/vite.config.ts
@@ -1,14 +1,16 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
-import { env } from "process";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
-const target = env.VITE_APP_API_TARGET ?? "http://automotive.marketplace.server:8080";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const target =
+    env.VITE_APP_PROXY_API_TARGET ??
+    "http://automotive.marketplace.server:8080";
+
   return {
     plugins: [
       TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
