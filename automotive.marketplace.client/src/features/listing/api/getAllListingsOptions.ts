5a153897aeaed5b2a6417941c8251cac143d5a95
taurasbear <youngicorn@gmail.com>
2025-08-27 13:44:30 +0100

    refactor: pass generic parameter instead of defining function type for axios request functions

--- automotive.marketplace.client/src/features/listing/api/getAllListingsOptions.ts
+++ automotive.marketplace.client/src/features/listing/api/getAllListingsOptions.ts
@@ -1,15 +1,14 @@
 import { ENDPOINTS } from "@/constants/endpoints";
 import axiosClient from "@/lib/axios/axiosClient";
 import { queryOptions } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
 import { GetAllListingsQuery } from "../types/GetAllListingsQuery";
 import { GetAllListingsResponse } from "../types/GetAllListingsResponse";
 import { listingKeys } from "./listingKeys";
 
-const getAllListings = (
-  query: GetAllListingsQuery,
-): Promise<AxiosResponse<GetAllListingsResponse[]>> =>
-  axiosClient.get(ENDPOINTS.LISTING.GET_ALL, { params: query });
+const getAllListings = (query: GetAllListingsQuery) =>
+  axiosClient.get<GetAllListingsResponse[]>(ENDPOINTS.LISTING.GET_ALL, {
+    params: query,
+  });
 
 export const getAllListingsOptions = (query: GetAllListingsQuery) =>
   queryOptions({
--- automotive.marketplace.client/src/features/search/api/getAllMakesOptions.ts
+++ automotive.marketplace.client/src/features/search/api/getAllMakesOptions.ts
@@ -1,12 +1,11 @@
 import { ENDPOINTS } from "@/constants/endpoints";
 import axiosClient from "@/lib/axios/axiosClient";
 import { queryOptions } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
-import { makeKeys } from "./makeKeys";
 import { GetAllMakesResponse } from "../types/GetAllMakesResponse";
+import { makeKeys } from "./makeKeys";
 
-const getAllMakes = (): Promise<AxiosResponse<GetAllMakesResponse[]>> =>
-  axiosClient.get(ENDPOINTS.MAKE.GET_ALL);
+const getAllMakes = () =>
+  axiosClient.get<GetAllMakesResponse[]>(ENDPOINTS.MAKE.GET_ALL);
 
 export const getAllMakesOptions = queryOptions({
   queryKey: makeKeys.all(),
