53476fa24d1a3017cb7a05e1271e9ebbd58dd458
taurasbear <youngicorn@gmail.com>
2025-09-19 16:19:45 +0300

    feat: add more validation constants add more validation helper functions

--- automotive.marketplace.client/src/constants/validation.ts
+++ automotive.marketplace.client/src/constants/validation.ts
@@ -17,4 +17,30 @@ export const VALIDATION = {
     MIN: 6,
     MAX: 100,
   },
-};
+  PRICE: {
+    MIN: 50,
+    MAX: 10_000_000,
+  },
+  POWER: {
+    MIN: 5,
+    MAX: 1000,
+  },
+  ENGINE_SIZE: {
+    MIN: 300,
+    MAX: 10000,
+  },
+  MILEAGE: {
+    MIN: 0,
+    MAX: 10_000_000,
+  },
+  YEAR: {
+    MIN: 1900,
+  },
+  VIN: {
+    REGEX: /^[A-HJ-NPR-Z0-9]{17}$/i,
+  },
+  GUID: {
+    REGEX:
+      /^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$/,
+  },
+} as const;
--- automotive.marketplace.client/src/utils/validation.ts
+++ automotive.marketplace.client/src/utils/validation.ts
@@ -6,4 +6,24 @@ export const validation = {
 
   maxLength: ({ label, length }: { label: string; length: number }) =>
     `${label} must be ${length} or fewer characters long`,
+
+  minSize: ({
+    label,
+    size,
+    unit,
+  }: {
+    label: string;
+    size: number;
+    unit?: string;
+  }) => `${label} must be at least ${size}${unit ? ` ${unit}` : ""}`,
+
+  maxSize: ({
+    label,
+    size,
+    unit,
+  }: {
+    label: string;
+    size: number;
+    unit?: string;
+  }) => `${label} must be ${size}${unit ? ` ${unit}` : ""} or less`,
 };
