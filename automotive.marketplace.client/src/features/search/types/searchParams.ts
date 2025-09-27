7972d6eb0609ef86b1918778a44b7a0ee5a5814c
taurasbear <youngicorn@gmail.com>
2025-09-27 21:28:43 +0300

    feat: move SearchParam type to a file

---
+++ automotive.marketplace.client/src/features/search/types/searchParams.ts
@@ -0,0 +1,10 @@
+export type SearchParams = {
+  makeId?: string;
+  modelId?: string;
+  city?: string;
+  isUsed?: boolean;
+  yearFrom?: number;
+  yearTo?: number;
+  priceFrom?: number;
+  priceTo?: number;
+};
