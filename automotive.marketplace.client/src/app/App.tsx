4919ccba5679464ace2d219c4bcac810152f9365
taurasbear <youngicorn@gmail.com>
2025-09-28 14:13:58 +0300

    feat: implement theme toggle button

--- automotive.marketplace.client/src/app/App.tsx
+++ automotive.marketplace.client/src/app/App.tsx
@@ -1,13 +1,14 @@
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { router } from "@/lib/router";
 import { RouterProvider } from "@tanstack/react-router";
 
 function App() {
   return (
-    <div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
-    </div>
+    </ThemeProvider>
   );
 }
 
