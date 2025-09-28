85b96a5d6dfdefef7dc7ef526825d275c9e62b79
taurasbear <youngicorn@gmail.com>
2025-09-28 14:29:03 +0300

    feat: move header to separate component file

--- automotive.marketplace.client/src/app/routes/__root.tsx
+++ automotive.marketplace.client/src/app/routes/__root.tsx
@@ -1,10 +1,6 @@
-import { LogoutButton } from "@/features/auth";
+import Header from "@/components/layout/header/Header";
 import { QueryClient } from "@tanstack/react-query";
-import {
-  createRootRouteWithContext,
-  Link,
-  Outlet,
-} from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 interface RouterContext {
@@ -14,25 +10,7 @@ interface RouterContext {
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
-      <div className="shadow-lg/2">
-        <div className="mx-8 flex items-center justify-between py-2 xl:mx-auto xl:max-w-6xl">
-          <div className="space-x-2 truncate">
-            <Link to="/" className="[&.active]:font-bold">
-              Home
-            </Link>{" "}
-            <Link to="/about" className="[&.active]:font-bold">
-              About
-            </Link>
-            <Link to="/listing/create" className="[&.active]:font-bold">
-              Create listing
-            </Link>
-          </div>
-          <div className="">
-            <LogoutButton />
-          </div>
-        </div>
-      </div>
-      {/* <hr /> */}
+      <Header />
       <div className="mx-8 xl:mx-auto xl:max-w-6xl">
         <Outlet />
         <TanStackRouterDevtools />
