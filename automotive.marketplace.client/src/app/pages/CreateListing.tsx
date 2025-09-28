c56962668775f3bbbf94312dbe9186aaf7832b23
taurasbear <youngicorn@gmail.com>
2025-09-28 19:14:23 +0300

    feat: add card background color for listing creation form

--- automotive.marketplace.client/src/app/pages/CreateListing.tsx
+++ automotive.marketplace.client/src/app/pages/CreateListing.tsx
@@ -4,7 +4,7 @@ const CreateListing = () => {
   return (
     <div className="my-24 flex flex-col items-center space-y-12">
       <h1 className="text-xl font-semibold">Create your own listing</h1>
-      <CreateListingForm className="border-border w-full rounded-2xl border-1 p-4" />
+      <CreateListingForm className="border-border bg-card w-full rounded-2xl border-1 p-4" />
     </div>
   );
 };
