9af301f1aed8bd33dee33b9662641cd76cde77ed
taurasbear <youngicorn@gmail.com>
2025-09-29 15:00:24 +0300

    refactor: move listing list to its own component

--- automotive.marketplace.client/src/app/pages/Listings.tsx
+++ automotive.marketplace.client/src/app/pages/Listings.tsx
@@ -1,23 +1,15 @@
 import { Route } from "@/app/routes/listings";
 import MakeSelect from "@/components/forms/MakeSelect";
 import { CarConditionKey } from "@/constants/carConditions";
-import { getAllListingsOptions, ListingCard } from "@/features/listingList";
+import ListingList from "@/features/listingList/components/ListingList";
 import UsedSelect from "@/features/search/components/UsedSelect";
-import { useSuspenseQuery } from "@tanstack/react-query";
 
 const Listings = () => {
   const searchParams = Route.useSearch();
 
-  const { data: listingsQuery } = useSuspenseQuery(
-    getAllListingsOptions(searchParams),
-  );
-
-  const listings = listingsQuery.data;
-
   return (
     <div className="flex w-full items-start justify-start space-x-24">
       <div className="border-border bg-secondary mt-64 flex-1 border-1 p-4">
-        <span>Im right here</span>
         <div className="border-border space-y-4 rounded-2xl border-1 p-2">
           <MakeSelect
             isAllMakesEnabled={false}
@@ -31,11 +23,7 @@ const Listings = () => {
           />
         </div>
       </div>
-      <div className="bg-background text-on-background flex w-188 flex-col gap-10">
-        {listings.map((l) => (
-          <ListingCard key={l.id} listing={l} />
-        ))}
-      </div>
+      <ListingList listingSearchQuery={searchParams} />
     </div>
   );
 };
---
+++ automotive.marketplace.client/src/features/listingList/components/ListingList.tsx
@@ -0,0 +1,25 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getAllListingsOptions } from "../api/getAllListingsOptions";
+import { GetAllListingsQuery } from "../types/GetAllListingsQuery";
+import ListingCard from "./ListingCard";
+
+type ListingListProps = {
+  listingSearchQuery: GetAllListingsQuery;
+};
+
+const ListingList = ({ listingSearchQuery }: ListingListProps) => {
+  const { data: listingsQuery } = useSuspenseQuery(
+    getAllListingsOptions(listingSearchQuery),
+  );
+
+  const listings = listingsQuery.data;
+  return (
+    <div className="bg-background text-on-background flex w-188 flex-col gap-10">
+      {listings.map((l) => (
+        <ListingCard key={l.id} listing={l} />
+      ))}
+    </div>
+  );
+};
+
+export default ListingList;
