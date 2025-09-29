953b8791ee43be9aa843b14548695f862f615f6d
taurasbear <youngicorn@gmail.com>
2025-09-29 21:54:57 +0300

    feat: improve listingSearchSchema

--- automotive.marketplace.client/src/features/search/schemas/listingSearchSchema.ts
+++ automotive.marketplace.client/src/features/search/schemas/listingSearchSchema.ts
@@ -1,12 +1,13 @@
+import { VALIDATION } from "@/constants/validation";
 import { z } from "zod";
 
 export const ListingSearchSchema = z.object({
-  makeId: z.string().optional().catch(undefined),
-  modelId: z.string().optional().catch(undefined),
+  makeId: z.string().regex(VALIDATION.GUID.REGEX).optional().catch(undefined),
+  modelId: z.string().regex(VALIDATION.GUID.REGEX).optional().catch(undefined),
   city: z.string().optional().catch(undefined),
   isUsed: z.boolean().optional().catch(undefined),
-  yearFrom: z.coerce.number().optional().catch(undefined),
-  yearTo: z.coerce.number().optional().catch(undefined),
-  priceFrom: z.coerce.number().optional().catch(undefined),
-  priceTo: z.coerce.number().optional().catch(undefined),
+  yearFrom: z.coerce.number().positive().optional().catch(undefined),
+  yearTo: z.coerce.number().positive().optional().catch(undefined),
+  priceFrom: z.coerce.number().positive().optional().catch(undefined),
+  priceTo: z.coerce.number().positive().optional().catch(undefined),
 });
