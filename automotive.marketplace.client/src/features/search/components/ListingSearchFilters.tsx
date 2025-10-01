f612e6a7a4acfe3c6b1dd300cab4cd3ebd79bd40
taurasbear <youngicorn@gmail.com>
2025-10-01 13:45:22 +0300

    fix: border styling for select inputs in listing search filter component

--- automotive.marketplace.client/src/features/search/components/ListingSearchFilters.tsx
+++ automotive.marketplace.client/src/features/search/components/ListingSearchFilters.tsx
@@ -23,16 +23,11 @@ const ListingSearchFilters = ({
   className,
 }: ListingSearchFiltersProps) => {
   return (
-    <div
-      className={cn(
-        "bg-background overflow-hidden rounded-2xl border-1",
-        className,
-      )}
-    >
-      <div className="grid grid-cols-1 divide-y-1 border-b-1 sm:grid-cols-3 sm:divide-x-1 sm:divide-y-0">
+    <div className={cn(className)}>
+      <div className="border-input divide-input grid grid-cols-1 divide-y-1 border-b sm:grid-cols-3 sm:divide-x-1 sm:divide-y-0">
         <div className="col-span-1">
           <MakeSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none rounded-tl-md rounded-tr-md border border-b-0 shadow-none sm:rounded-tr-none sm:border-r-0 sm:border-b-0"
             label="Make"
             isAllMakesEnabled={true}
             value={searchValues.makeId}
@@ -41,7 +36,7 @@ const ListingSearchFilters = ({
         </div>
         <div className="col-span-1">
           <ModelSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none border-0 border-x shadow-none sm:border-x-0 sm:border-t"
             isAllModelsEnabled={true}
             defaultValue="all"
             label="Model"
@@ -54,16 +49,16 @@ const ListingSearchFilters = ({
         </div>
         <div className="col-span-1">
           <LocationCombobox
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none border-0 border-x shadow-none sm:rounded-tr-md sm:border-x-0 sm:border-t sm:border-r"
             value={searchValues.city}
             onValueChange={(value) => updateSearchValue("city", value)}
           />
         </div>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-6 sm:divide-x-1">
+      <div className="divide-input grid grid-cols-2 sm:grid-cols-6 sm:divide-x-1">
         <div className="col-span-2 border-b-1 sm:border-b-0">
           <UsedSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none border-0 border-x shadow-none sm:rounded-bl-md sm:border-x-0 sm:border-b-1 sm:border-l-1"
             value={searchValues.isUsed}
             defaultValue="newUsed"
             onValueChange={(value) => updateSearchValue("isUsed", value)}
@@ -71,7 +66,7 @@ const ListingSearchFilters = ({
         </div>
         <div className="border-r-1 border-b-1 sm:border-b-0">
           <YearSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none border-0 border-l shadow-none sm:border-b-1 sm:border-l-0"
             label="Min year"
             value={searchValues.minYear}
             onValueChange={(value) => updateSearchValue("minYear", value)}
@@ -79,7 +74,7 @@ const ListingSearchFilters = ({
         </div>
         <div className="border-b-1 sm:border-b-0">
           <YearSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none border-0 border-r shadow-none sm:border-r-0 sm:border-b-1"
             label="Max year"
             value={searchValues.maxYear}
             onValueChange={(value) => updateSearchValue("maxYear", value)}
@@ -87,7 +82,7 @@ const ListingSearchFilters = ({
         </div>
         <div className="border-r-1">
           <PriceSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none rounded-bl-md border-0 border-b border-l shadow-none sm:rounded-bl-none sm:border-l-0"
             label="Min price"
             value={searchValues.minPrice}
             onValueChange={(value) => updateSearchValue("minPrice", value)}
@@ -95,7 +90,7 @@ const ListingSearchFilters = ({
         </div>
         <div>
           <PriceSelect
-            className="min-h-15 border-0 shadow-none"
+            className="min-h-15 rounded-none rounded-br-md border-0 border-r border-b shadow-none"
             label="Max price"
             value={searchValues.maxPrice}
             onValueChange={(value) => updateSearchValue("maxPrice", value)}
