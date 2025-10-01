82e535b8aa33405a2d996fafc736254cd47d7146
taurasbear <youngicorn@gmail.com>
2025-10-01 20:42:01 +0300

    fix: margin bottom was placed on wrong container

--- automotive.marketplace.client/src/app/pages/Listings.tsx
+++ automotive.marketplace.client/src/app/pages/Listings.tsx
@@ -6,8 +6,8 @@ const Listings = () => {
   const navigate = Route.useNavigate();
 
   return (
-    <div className="mt-12 flex w-full items-start justify-start space-x-12">
-      <div className="mt-48 mb-24 flex-1">
+    <div className="mt-12 mb-24 flex w-full items-start justify-start space-x-12">
+      <div className="mt-48 flex-1">
         <Filters
           searchParams={searchParams}
           onSearchParamChange={(searchParams) =>
