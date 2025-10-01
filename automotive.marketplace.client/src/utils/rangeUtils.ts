8d4f156900f6bec18860e6dfb21bbe69e547531b
taurasbear <youngicorn@gmail.com>
2025-10-01 18:46:10 +0300

    feat: add range utils

---
+++ automotive.marketplace.client/src/utils/rangeUtils.ts
@@ -0,0 +1,47 @@
+export const rangeUtils = {
+  getPriceRange: (): string[] => {
+    const prices = ["150", "300", "500"];
+    for (let i = 1000; i < 5000; i += 500) {
+      prices.push(i.toString());
+    }
+    for (let i = 5000; i < 10000; i += 1000) {
+      prices.push(i.toString());
+    }
+    for (let i = 10000; i <= 100000; i += 2500) {
+      prices.push(i.toString());
+    }
+
+    return prices;
+  },
+  getMileageRange: (): string[] => {
+    const mileages = ["0", "2500", "5000"];
+    for (let i = 10000; i < 100_000; i += 10_000) {
+      mileages.push(i.toString());
+    }
+    for (let i = 100_000; i < 1_000_000; i += 20_000) {
+      mileages.push(i.toString());
+    }
+
+    return mileages;
+  },
+  getPowerRange: (): string[] => {
+    const powerValues = ["1"];
+    for (let i = 10; i < 100; i += 10) {
+      powerValues.push(i.toString());
+    }
+    for (let i = 100; i <= 400; i += 20) {
+      powerValues.push(i.toString());
+    }
+
+    return powerValues;
+  },
+  getYearRange: (): string[] => {
+    const years = [];
+    const currentYear = new Date().getUTCFullYear();
+    for (let i = 1950; i <= currentYear; i += 1) {
+      years.push(i.toString());
+    }
+
+    return years;
+  },
+};
