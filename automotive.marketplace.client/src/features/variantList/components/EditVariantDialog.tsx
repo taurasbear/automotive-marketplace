f4ad0264da6930173c8d68fe4a0eb1b23ecdb03e
taurasbear <youngicorn@gmail.com>
2026-04-19 21:29:32 +0300

    feat: add EditVariantDialog and EditVariantDialogContent

---
+++ automotive.marketplace.client/src/features/variantList/components/EditVariantDialog.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import { useUpdateVariant } from "../api/useUpdateVariant";
+import { Variant } from "../types/Variant";
+import { VariantFormData } from "../types/VariantFormData";
+import EditVariantDialogContent from "./EditVariantDialogContent";
+
+type EditVariantDialogProps = {
+  variant: Variant;
+  makeId: string;
+};
+
+const EditVariantDialog = ({ variant, makeId }: EditVariantDialogProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { mutateAsync: updateVariantAsync } = useUpdateVariant();
+
+  const handleSubmit = async (formData: VariantFormData) => {
+    const { makeId: _, ...command } = formData;
+    await updateVariantAsync({ id: variant.id, ...command });
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button variant="secondary">
+          <Pencil />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <EditVariantDialogContent
+          variant={variant}
+          makeId={makeId}
+          onSubmit={handleSubmit}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EditVariantDialog;
