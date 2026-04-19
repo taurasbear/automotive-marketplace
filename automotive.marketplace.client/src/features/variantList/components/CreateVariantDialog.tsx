f010df064a6e1c8be3cd0f7c62fc5e778527026c
taurasbear <youngicorn@gmail.com>
2026-04-19 21:28:46 +0300

    feat: add CreateVariantDialog

---
+++ automotive.marketplace.client/src/features/variantList/components/CreateVariantDialog.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { useCreateVariant } from "../api/useCreateVariant";
+import { VariantFormData } from "../types/VariantFormData";
+import VariantForm from "./VariantForm";
+
+type CreateVariantDialogProps = {
+  modelId: string;
+  makeId: string;
+};
+
+const CreateVariantDialog = ({ modelId, makeId }: CreateVariantDialogProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { mutateAsync: createVariantAsync } = useCreateVariant();
+
+  const handleSubmit = async (formData: VariantFormData) => {
+    const { makeId: _, ...command } = formData;
+    await createVariantAsync(command);
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button>Add variant</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create new variant</DialogTitle>
+        </DialogHeader>
+        <VariantForm
+          variant={{
+            makeId,
+            modelId,
+            year: new Date().getFullYear(),
+            fuelId: "",
+            transmissionId: "",
+            bodyTypeId: "",
+            doorCount: 4,
+            powerKw: 100,
+            engineSizeMl: 1600,
+            isCustom: false,
+          }}
+          onSubmit={handleSubmit}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateVariantDialog;
